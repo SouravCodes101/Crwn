bffd54116fd1e4f25213d9d0e50cedafaf0c7827
Sourav <sourav.choudhury@positra.com>
2023-05-10 17:02:46 +0530

    Get Products + Categories from FireStore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,10 @@
 import { createContext, useEffect, useState } from "react";
 
-import PRODUCTS from "../shop-data.json"
+// import { addCollectionsAndDocuments } from "../utils/firebase/firebase.utils";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+// import SHOP_DATA from "../shop-data.js";
 
 // The actual value that we want to access
 export const ProductsContext = createContext({
@@ -8,7 +12,17 @@ export const ProductsContext = createContext({
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   addCollectionsAndDocuments("categories", SHOP_DATA);
+  // },[])
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments()
+      console.log(categoryMap);
+    }
+    getCategoriesMap();
+  }, [])
   const value = { products };
 
   return (
